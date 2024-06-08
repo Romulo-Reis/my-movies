706b7569459a34120d099aacca4dd88bb302d4ff
Romulo-Reis <romuloreistavares@hotmail.com>
2024-06-08 13:09:32 -0300

    refactor: Adiciona a propriedade key no componente MovieItem.

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,7 +2,7 @@ import { MovieItem } from './MovieItem';
 export const MoviesList = ({ movies }) => (
     <section>
         <ul>
-            { movies.map(m => <MovieItem title ={m.title } />)}
+            { movies.map(m => <MovieItem key={ m.title } title={ m.title } />)}
         </ul>
     </section>
 );
