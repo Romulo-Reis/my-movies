e8fb4ad3f98340a49b5edffc27d6c56e55ce0d5b
Romulo-Reis <romuloreistavares@hotmail.com>
2024-06-08 13:11:22 -0300

    feat: Cria a função  fetchMovies.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { MoviesList } from "./components/MoviesList";
 import { MoviesContainer } from "./styles/MoviesContainer";
+import { MoviesService } from "./api/MoviesService";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const fetchMovies = async () => {
+    const { data } = await MoviesService.getMovies();
+    setMovies(data.results);
+  }
+
+  useEffect(() => {
+    fetchMovies()
+    console.log(movies)
+  },[])
+
   return (
     <div className="App">
       <Header>
         <MoviesContainer>
-          <MoviesList movies={[]} />
+          <MoviesList movies={movies} />
         </MoviesContainer>
       </Header>
     </div>
